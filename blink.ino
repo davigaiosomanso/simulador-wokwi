8aa21eaf32e2c1ac2953efab79187488bd5c003b
davigaiosomanso <davimanso@usp.br>
2026-04-24 09:46:15 -0300

    feat: adicionou leds amarela e verde no blink.ino

--- blink.ino
+++ blink.ino
@@ -26,12 +26,23 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(3, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
+  digitalWrite(8, LOW);
+  digitalWrite(3, LOW);
+  delay(3000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(8, LOW);
+  digitalWrite(3, HIGH);
+  delay(3000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(3, LOW);
+  delay(1000);
+
 }
